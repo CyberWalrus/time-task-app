3a54c983756fbca5ad2894ffdc2577cd536229f7
Cyber Walrus <stonewalrusfan@gmail.com>
2019-07-30 18:08:24 +0300

    custom select add find item logic

--- client/components/app/app.tsx
+++ client/components/app/app.tsx
@@ -3,14 +3,14 @@ import Header from '../header/header';
 import FormTask from '../form-task/form-task';
 import CustomSelect from '../custom-select/custom-select';
 
-const customLists = [{ test: 'hello' }, { walrus: 'cyber' }];
+const customLists = [{ value: 'TOP', text: 'HEllo' }, { value: 'BOTTOM', text: 'test' }];
 
 const App: FunctionComponent = (): ReactElement => (
   <Fragment>
     <Header />
     <main className="page-content">
       <FormTask />
-      <CustomSelect list={customLists} />
+      <CustomSelect items={customLists} />
     </main>
   </Fragment>
 );
--- client/components/custom-select/custom-select.tsx
+++ client/components/custom-select/custom-select.tsx
@@ -1,37 +1,64 @@
 import React, {
-  FC, ReactElement, useState, ChangeEvent, useCallback,
+  FC, ReactElement, useState, ChangeEvent, useCallback, useEffect,
 } from 'react';
 import { Option } from 'client/types/forms';
 
+interface Item {
+  value: string;
+  text: string;
+}
 interface Props {
   option?: Option;
   value?: Record<string, string>;
-  list?: Record<string, string>[];
+  items?: Item[];
 }
 interface State {
   value: string;
+  selected: string;
+  items: Item[];
 }
-const defaultState: State = { value: '' };
+const defaultState: State = { value: '', selected: '', items: [] };
 
-const CustomSelect: FC<Props> = ({ list }: Props): ReactElement => {
-  const [state, setState] = useState<State>(defaultState);
+const CustomSelect: FC<Props> = ({ items }: Props): ReactElement => {
+  const [state, setState] = useState<State>({ ...defaultState, items });
+  const updateList = useCallback((): void => {
+    const itemsNew = items
+      .slice(0)
+      .filter((item: Item): boolean => item.text.toLowerCase().indexOf(state.value.toLowerCase()) >= 0);
+    setState({ ...state, items: itemsNew });
+  }, [items, state]);
   const handleChangeValue = useCallback(
     (event: ChangeEvent<HTMLInputElement>): void => {
       setState({ ...state, value: event.target.value });
     },
     [state],
   );
+  const handleChangeSelected = useCallback(
+    (selected: Item): void => {
+      setState({ ...state, selected: selected.value, value: selected.text });
+    },
+    [state],
+  );
+  useEffect((): void => {
+    updateList();
+  }, [state.value, updateList]);
   return (
     <div className="custom-select">
       <div className="custom-select__header">
+        {state.selected}
         <input type="text" value={state.value} onChange={handleChangeValue} className="custom-select__input" />
+        <button type="button" onClick={(): void => handleChangeSelected({ value: '', text: '' })}>
+          Clear
+        </button>
       </div>
       <ul className="custom-select__list">
-        {list
-          && list.map(
-            (item: Record<string, string>): ReactElement => (
-              <li key={`custom-select-${Object.keys(item)[0]}`} className="custom-select__item">
-                {Object.values(item)[0]}
+        {state.items
+          && state.items.map(
+            (item: Item): ReactElement => (
+              <li key={`custom-select-${item.value}`} className="custom-select__item">
+                <button type="button" onClick={(): void => handleChangeSelected(item)}>
+                  {item.text}
+                </button>
               </li>
             ),
           )}
